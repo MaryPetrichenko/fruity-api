c4e94fb3ddb5d4acec491a7fcf2ce8af19be7f87
Mary <haritmash@yandex.ru>
2025-01-21 09:57:16 +0000

    Add cors to the project

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 const logger = require("./logger")
 
 //middleware
 app.use(express.json())
-app.use(logger)  // will run on every pat
+app.use(logger)  // will run on every path
+app.use(cors)
 
 const fruitsRouter = require("./routes/fruits")
 
